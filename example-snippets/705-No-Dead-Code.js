40a1b104d781b7ecaaa7b4a0bcb781bd560d1bba
unknown <steen_hansen@yahoo.com>
2023-12-29 13:19:32 -0800

    Update 705-No-Dead-Code.js

--- example-snippets/705-No-Dead-Code.js
+++ example-snippets/705-No-Dead-Code.js
@@ -2,18 +2,20 @@
 
 //  https://cdn.jsdelivr.net/gh/steenhansen/type-czech@latest/example-snippets/705-No-Dead-Code.js
 
-if (typeof TypeCzech === 'function') {
+if (typeof TypeCzech !== 'function') {
+  linkUp_705 = () => { }
+} else {
     linkUp_705 = () => {
-    type_czech = TypeCzech('LOG-ERRORS');
+      type_czech = TypeCzech('LOG-ERRORS')
 
-    function PRE_addInt2Int(first_int, second_int){
-      return type_czech.checkParam_type([first_int, second_int], ['number', 'number'])
-    }
+      function PRE_addInt2Int(first_int, second_int) {
+        return type_czech.checkParam_type([first_int, second_int], ['number', 'number'])
+      }
 
-    function POST_addInt2Int(sum_int) {
-            return type_czech.checkParam_type(sum_int, 'number')
-    }
+      function POST_addInt2Int(sum_int) {
+        return type_czech.checkParam_type(sum_int, 'number')
+      }
    
-    addInt2Int = type_czech.linkUp(addInt2Int, PRE_addInt2Int, POST_addInt2Int)
-  };
+      addInt2Int = type_czech.linkUp(addInt2Int, PRE_addInt2Int, POST_addInt2Int)
+    };
 }
